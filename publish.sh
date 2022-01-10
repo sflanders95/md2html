386230f92dc78026be5ec768b5d01c91d968d120
Steven Flanders <sflanders95@gmail.com>
2022-01-10 16:05:59 -0500

    addedLinux

--- publish.sh
+++ publish.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
+# MacOS
 dotnet publish --configuration Release --framework netcoreapp3.1 --runtime osx.10.12-x64 --self-contained true md2html.csproj
+
+# uncomment for linux
+# dotnet publish --configuration Release --framework netcoreapp3.1 --runtime linux-x64 --self-contained true md2html.csproj
 
